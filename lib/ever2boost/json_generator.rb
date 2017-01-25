c9e712beaa052860424548a486f7daef25f0259d
asmsuechan <suenagaryoutaabc@gmail.com>
2017-01-25 16:02:44 +0900

    Change the structure of JsonGenerator
    assign color as instance variable

--- lib/ever2boost/json_generator.rb
+++ lib/ever2boost/json_generator.rb
@@ -1,45 +1,29 @@
 module Ever2boost
   class JsonGenerator
-    FOLDER_COLORS = [
-      '#E10051',
-      '#FF8E00',
-      '#E8D252',
-      '#3FD941',
-      '#30D5C8',
-      '#2BA5F7',
-      '#B013A4'
-    ].freeze
-
     class << self
       def build(notebook_list)
         folders = <<-EOS
         EOS
 
         notebook_list.each do |list|
-          random_color = self.random_color
           brace = notebook_list.last == list ? '}' : '},'
           folders += <<-EOS
-            {
-              "key": "#{list.hash}",
-              "name": "#{list.title}",
-              "color": "#{random_color}"
-            #{brace}
+    {
+      "key": "#{list.hash}",
+      "name": "#{list.title}",
+      "color": "#{list.color}"
+    #{brace}
           EOS
         end
 
         json = <<-EOS
-          {
-            "folders": [
-              #{folders}
-            ],
-            "version": "1.0"
-          }
+{
+  "folders": [
+#{folders.chomp}
+  ],
+  "version": "1.0"
+}
         EOS
-      end
-
-      def random_color
-        index =  ((Random.new().rand * 7)*10.floor % 7).to_i
-        FOLDER_COLORS[index]
       end
 
       def output(notebook_list, output_dir)
--- lib/ever2boost/note_list.rb
+++ lib/ever2boost/note_list.rb
@@ -4,12 +4,26 @@ module Ever2boost
   class NoteList
     DEFAULT_BYTES_NUMBER = 6
 
-    attr_accessor :title, :hash, :guid
+    FOLDER_COLORS = [
+      '#E10051',
+      '#FF8E00',
+      '#E8D252',
+      '#3FD941',
+      '#30D5C8',
+      '#2BA5F7',
+      '#B013A4'
+    ].freeze
 
-    def initialize(title: nil, hash: nil, guid: nil)
+    attr_accessor :title, :hash, :guid, :color
+
+    def initialize(title: nil, hash: nil, guid: nil, color: nil)
+      index =  ((Random.new().rand * 7)*10.floor % 7).to_i
+      color = FOLDER_COLORS[index]
+
       @title = title
       @hash = SecureRandom.hex(DEFAULT_BYTES_NUMBER)
       @guid = guid
+      @color = color
     end
   end
 end
