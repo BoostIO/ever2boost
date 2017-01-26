cb835f96f5995ee5aac93b6ed9ec761a1f3f4cfb
asmsuechan <suenagaryoutaabc@gmail.com>
2017-01-26 09:10:06 +0900

    Add a option which changes the output directory

--- lib/ever2boost/cli.rb
+++ lib/ever2boost/cli.rb
@@ -3,12 +3,14 @@ require 'ever2boost/evernote_authorizer'
 
 module Ever2boost
   class CLI < Thor
-    DEFAULT_OUTPUT_DIR = "#{ENV['HOME']}/ever2boost_store"
+    DEFAULT_OUTPUT_DIR = "#{ENV['HOME']}/evernote_storage"
 
     desc "import", "import from evernote"
+    option :directory, aliases: :d, banner: 'DIRCTORY_PATH', desc: "make Boostnote storage in the directory\ndefault: ~/evernote_storage"
     def import
+      output_dir = options[:directory] || DEFAULT_OUTPUT_DIR
       developer_token = ask('DEVELOPER_TOKEN:')
-      EvernoteAuthorizer.new(developer_token).import(DEFAULT_OUTPUT_DIR)
+      EvernoteAuthorizer.new(developer_token).import(output_dir)
     end
   end
 end
