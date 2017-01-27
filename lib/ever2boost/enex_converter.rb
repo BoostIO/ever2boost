3a46eee9aa78205552da45a250ec7094a4cff0c3
asmsuechan <suenagaryoutaabc@gmail.com>
2017-01-27 10:06:30 +0900

    Add STDOUT on converting .enex

--- lib/ever2boost/enex_converter.rb
+++ lib/ever2boost/enex_converter.rb
@@ -4,20 +4,21 @@ module Ever2boost
   class EnexConverter
     class << self
       def convert(enex, output_dir, filename)
+        puts 'converting...'
         en_notes = []
         REXML::Document.new(Ever2boost::MdConverter.convert(enex)).elements['en-export'].each { |el| en_notes.push(el.to_s) }
         en_notes.delete("\n")
         notes = en_notes.map do |note|
           title = REXML::Document.new(note).elements['note/title'].text
+          puts "converting #{title}"
           Note.new(title: title, content: "<div>#{REXML::Document.new(note).elements['note/content/text()'].value.sub(/.+\n/, '').sub(/.+\n/, '')}</div>")
         end
         notebook_list = [NoteList.new(title: filename)]
-        puts 'converting...'
         JsonGenerator.output(notebook_list, output_dir)
         notes.each do |note|
           CsonGenerator.output(notebook_list.first.hash, note, output_dir)
         end
-        puts 'Success!'
+        puts "\e[32mSuccess!\nThe notes are created at #{output_dir}\e[0"
       end
     end
   end
