f2505a6a0e4afcbb3a0410d34fb12b833090a879
narukami894 <shunsukeyakushiji7@gmail.com>
2018-02-27 19:27:34 +0900

    generate json pretty

--- spec/json_generator_spec.rb
+++ spec/json_generator_spec.rb
@@ -5,7 +5,7 @@ describe Ever2boost::JsonGenerator do
   let (:notelist1) { Ever2boost::NoteList.new(title: 'title1', guid: '012345abcdef') }
   let (:notelist2) { Ever2boost::NoteList.new(title: 'title2', guid: '0123456abcde') }
   let (:notebook_list) { [notelist1, notelist2] }
-  let (:json) { '{"folders":[{"key":"012345abcdef","name":"title1","color":"#E10051"},{"key":"0123456abcde","name":"title2","color":"#E10051"}],"version":"1.0"}' }
+  let (:json) { "{\n  \"folders\": [\n    {\n      \"key\": \"012345abcdef\",\n      \"color\": \"#E10051\",\n      \"name\": \"title1\"\n    },\n    {\n      \"key\": \"0123456abcde\",\n      \"color\": \"#E10051\",\n      \"name\": \"title2\"\n    }\n  ],\n  \"version\": \"1.0\"\n}" }
   let (:output_dir) { 'spec/dist/evernote_storage' }
 
   describe '#build' do
